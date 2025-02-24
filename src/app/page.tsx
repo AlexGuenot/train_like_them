d757a25b14b351ec753c34cbf8ac4bc4743c2f8d
AlexGuenot <stoapofftrack@gmail.com>
2025-02-24 20:17:41 +0100

    +_

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return(
+    <div className="flex flex-col items-center">
+      <h1 className="text-bold text-3xl">Next App Created</h1>
+      <button className="btn btn-success">Button here</button>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+import daisyui from "daisyui"
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  daisyui: {
+    themes: ["retro", "dark","luxury"],
+  },
+  plugins: [
+    daisyui
+  ],
+} satisfies Config;
